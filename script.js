6689a73c37aea9080b63b1288e3373fb36c6761b
irdepla <rixsiboyevislombek@gmail.com>
2024-09-29 23:20:30 +0500

    done

---
+++ script.js
@@ -0,0 +1,346 @@
+// For 1
+
+// let k = +prompt("Son kiriting:");
+// let n = 2;
+
+// for (let i = 1; i <= n; i++) {
+//   console.log(k);
+// }
+
+
+// For 2
+
+// let a = +prompt("Son kiriting:");
+// let b = +prompt("Son kiriting:");
+// let s = 0
+
+
+// for(let i = a; i <= b; i++){
+//   console.log(i);
+//   s++
+// }
+
+// console.log(s);
+
+
+
+// For 3
+
+// let a = +prompt("Son kiriting:");
+// let b = +prompt("Son kiriting:");
+// let s = 0
+
+
+// for(let i = a - 1; i > b; i--){
+//   console.log(i);
+//   s++
+// }
+
+// console.log(s);
+
+// For 4
+
+// let a = +prompt("Son kiriting:");
+
+// for(let i = 1; i <= 10; i++){
+//   console.log(i * a);
+  
+// }
+
+
+
+// For 5
+
+// let k = +prompt("Son kiriting:");
+
+// for (let i = 0.1; i <= 1; i += 0.1){
+//     console.log(k*i);
+// }
+
+// For 6
+
+
+// let k = +prompt("Son kiriting:");
+
+// for (let i = 1.2; i <= 2; i += 0.2){
+//     console.log(k*i);
+// }
+
+
+
+// For 7
+
+
+// let a = +prompt("Son kiriting:");
+// let b = +prompt("Son kiriting:");
+// let s = 0
+
+// for(let i = a; i <= b; i++){
+//   s += i
+// }
+// console.log(a + " dan " + b + " gacha sonlar yig'indisi " + s + "ga teng.");
+
+
+// For 8
+
+
+// let a = +prompt("Son kiriting:");
+// let b = +prompt("Son kiriting:");
+// let s = 1
+
+// for(let i = a; i <= b; i++){
+//   s *= i
+// }
+// console.log(a + " dan " + b + " gacha sonlar ko'paytmasi " + s + "ga teng.");
+
+
+
+// For 9
+
+
+
+// let a = +prompt("Son kiriting:");
+// let b = +prompt("Son kiriting:");
+// let s = 0
+
+// for(let i = a; i <= b; i++){
+//   s += i**2
+// }
+// console.log(a + " dan " + b + " gacha bo'lgan sonlar kvadratlari yig'indisi " + s + "ga teng.");
+
+
+
+
+// For 10
+
+// let n = +prompt("Son kiriting:");
+// let s = 0
+
+// for(let i = 1; i <= n; i++ ){
+//     s += 1/i
+// }
+
+// console.log(s);
+
+// While 1
+
+// let n = +prompt("Son kiriting:");
+// let m = +prompt("Son kiriting:");
+
+// while(n >= m){
+//     n = n -= m
+// }
+// console.log(n);
+
+
+// While 2
+
+
+// let n = +prompt("Son kiriting:");
+// let m = +prompt("Son kiriting:");
+// let s = 0
+
+// while(n >= m){
+//     n -= m
+//     s++
+// }
+// console.log(s);
+
+
+// While 3
+
+
+
+// let n = +prompt("Son kiriting:");
+// let m = +prompt("Son kiriting:");
+// let s = 0
+
+// while(n >= m){
+//     n = n -= m
+//     s++
+// }
+// console.log("Butun qismi: " + s);
+// console.log("Qoldiq qismi: " + n);
+
+
+// While 4
+
+// let n = +prompt("nsj")
+// let s = 1
+// while(s < n){
+//   s = s * 3
+// }
+
+// if(s == n){
+//   console.log("3 ning darajasi");
+  
+// }
+// else{
+//   console.log("3 ning darajasi emas");
+  
+// }
+
+
+
+// While 5
+
+// let n = +prompt("Son kiriting:");
+// let m = +prompt("Son kiriting:");
+// while (n >= 2) {
+//   n /= 2;
+//   s++;
+// }
+// console.log(s);
+
+
+
+// While 17
+
+
+// let n = +prompt("Son kiriting:");
+// let m = +prompt("Son kiriting:");
+// let s = 0
+
+
+// while(n >= m){
+//   n -= m
+//   s++
+// }
+// console.log("Butun qismi: " + s);
+// console.log("Qoldiq qismi: " + n);
+
+
+// While 18
+
+
+// let a = +prompt("Son kiriting:")
+
+// let m = 0
+// let raqam = 0
+
+// while(a > 0){
+//   raqam = a % 10
+//   m = m * 10 + raqam
+//   a = Math.floor(a / 10); 
+// }
+
+// console.log(m);
+
+
+// While 19
+
+
+// let a = +prompt("Son kiriting: ")
+
+
+// let s = 0
+// let x = 0
+
+// while(a > 0){
+//   s += a % 10;   
+//   a = Math.floor(a / 10)
+//   x++
+// }
+
+
+// console.log("Raqamlari yig'indisi "+ s);
+// console.log("Raqamlari soni "+ x);
+
+
+
+// While 20
+
+
+
+// let a = +prompt("Son kiriting: ")
+
+
+// let s = 0
+
+// while(a > 0){
+//   s = a % 10;
+//   if(s == 2){
+//     console.log("Ikki raqami bor:");
+//   }
+//   else{
+//     console.log("Ikki raqami yo'q:");
+    
+//   }
+//   a = Math.floor(a / 10)
+// }
+
+
+
+
+
+// Vundekind
+
+// For 11
+
+
+// let n = +prompt("Son kiriting: ")
+// let s = 0
+
+// for(let i = n; i <= 2 * n; i++ ){
+//   s += i ** 2
+// }
+// console.log(s);
+
+
+
+// For 12
+
+
+// let n = +prompt("Sn kiriting: ");
+// let s = 1.1;
+
+// if (n > 1) {
+//     for (let i = 1.2; i <= 1 + (n / 10); i += 0.1) {
+//         s *= i;
+//     }
+// }
+
+// console.log(s);
+
+
+
+// While 21
+
+
+// let n = +prompt("Son kiriting: ");
+
+// let r1 = 0
+
+
+// while(n > 0){
+//   r1 += n % 10
+//   n = Math.floor(n / 10)
+//   if(r1 % 2 == 1){
+//     console.log("Toq raqam bor");
+//   }
+
+// }
+
+
+//  While 22 Ishlanmagan chala
+
+
+// let n = 3
+
+
+// if (n < 2){
+//   console.log("Tub son emas.");
+// }
+// else{
+//   while(){}
+// }
+
+
+// Modulli tenglama
+
+
+// let x = +prompt("Son kiriting: ");
+
+// let y = Math.abs(2 * x - 3) == 7
+// console.log(y);
+
